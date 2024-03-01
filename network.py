b46c321d70186c4c28249f1be333042c29a8eb4a
Chinmay Mokashi <chinmaymokashicm@gmail.com>
2024-03-01 11:33:46 -0600

    Updated sample network.py

--- network.py
+++ network.py
@@ -1,64 +1,151 @@
-from typing import Optional
-
-from pydantic import BaseModel
+from torch import nn
+from torchsummary import summary
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
+import time
 
-class NetworkConfig(BaseModel):
-    num_classes: int = 3
-    input_channels: int = 1
+IMAGE_SIZE = [240, 240, 155]
+IMAGE_SIZE = [64, 64, 64]
+
+class Conv3DBlock(nn.Module):
+    """
+    The basic block for double 3x3x3 convolutions in the analysis path
+    -- __init__()
+    :param in_channels -> number of input channels
+    :param out_channels -> desired number of output channels
+    :param bottleneck -> specifies the bottlneck block
+    -- forward()
+    :param input -> input Tensor to be convolved
+    :return -> Tensor
+    """
+
+    def __init__(self, in_channels, out_channels, bottleneck = False) -> None:
+        super(Conv3DBlock, self).__init__()
+        self.conv1 = nn.Conv3d(in_channels= in_channels, out_channels=out_channels//2, kernel_size=(3,3,3), padding=1)
+        self.bn1 = nn.BatchNorm3d(num_features=out_channels//2)
+        self.conv2 = nn.Conv3d(in_channels= out_channels//2, out_channels=out_channels, kernel_size=(3,3,3), padding=1)
+        self.bn2 = nn.BatchNorm3d(num_features=out_channels)
+        self.relu = nn.ReLU()
+        self.bottleneck = bottleneck
+        if not bottleneck:
+            self.pooling = nn.MaxPool3d(kernel_size=(2,2,2), stride=2)
+
     
+    def forward(self, input):
+        res = self.relu(self.bn1(self.conv1(input)))
+        res = self.relu(self.bn2(self.conv2(res)))
+        out = None
+        if not self.bottleneck:
+            out = self.pooling(res)
+        else:
+            out = res
+        return out, res
+
+
+
+
+class UpConv3DBlock(nn.Module):
+    """
+    The basic block for upsampling followed by double 3x3x3 convolutions in the synthesis path
+    -- __init__()
+    :param in_channels -> number of input channels
+    :param out_channels -> number of residual connections' channels to be concatenated
+    :param last_layer -> specifies the last output layer
+    :param num_classes -> specifies the number of output channels for dispirate classes
+    -- forward()
+    :param input -> input Tensor
+    :param residual -> residual connection to be concatenated with input
+    :return -> Tensor
+    """
+
+    def __init__(self, in_channels, res_channels=0, last_layer=False, num_classes=None) -> None:
+        super(UpConv3DBlock, self).__init__()
+        assert (last_layer==False and num_classes==None) or (last_layer==True and num_classes!=None), 'Invalid arguments'
+        self.upconv1 = nn.ConvTranspose3d(in_channels=in_channels, out_channels=in_channels, kernel_size=(2, 2, 2), stride=2)
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm3d(num_features=in_channels//2)
+        self.conv1 = nn.Conv3d(in_channels=in_channels+res_channels, out_channels=in_channels//2, kernel_size=(3,3,3), padding=(1,1,1))
+        self.conv2 = nn.Conv3d(in_channels=in_channels//2, out_channels=in_channels//2, kernel_size=(3,3,3), padding=(1,1,1))
+        self.last_layer = last_layer
+        if last_layer:
+            self.conv3 = nn.Conv3d(in_channels=in_channels//2, out_channels=num_classes, kernel_size=(1,1,1))
+            
+        
+    def forward(self, input, residual=None):
+        out = self.upconv1(input)
+        if residual is not None:
+            # Adjust spatial dimensions of residual to match 'out'
+            residual = F.interpolate(residual, size=out.shape[2:], mode='trilinear', align_corners=False)
+            out = torch.cat((out, residual), 1)
+        out = self.relu(self.bn(self.conv1(out)))
+        out = self.relu(self.bn(self.conv2(out)))
+        if self.last_layer: out = self.conv3(out)
+        return out
+        
+
+
+
 class Network(nn.Module):
-    def __init__(self, config: Optional[NetworkConfig] = None) -> None:
-        super().__init__()
-        
-        if config is None:
-            config = NetworkConfig()
-        self.config = config
-        
-        # Convolutional layers
-        self.conv1 = nn.Conv3d(self.config.input_channels, 64, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv3d(64, 64, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool3d(kernel_size=2, stride=2)
-
-        self.conv3 = nn.Conv3d(64, 128, kernel_size=3, padding=1)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.conv4 = nn.Conv3d(128, 128, kernel_size=3, padding=1)
-        self.relu4 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool3d(kernel_size=2, stride=2)
-
-        # Fully connected layers
-        self.fc1 = nn.Linear(128 * 16 * 16 * 16, 512)
-        self.relu5 = nn.ReLU(inplace=True)
-        self.fc2 = nn.Linear(512, self.config.num_classes)
-        
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.pool1(x)
-
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = self.conv4(x)
-        x = self.relu4(x)
-        x = self.pool2(x)
-
-        x = x.view(x.size(0), -1)
-
-        x = self.fc1(x)
-        x = self.relu5(x)
-        x = self.fc2(x)
-
-        return x
+    """
+    The 3D UNet model
+    -- __init__()
+    :param in_channels -> number of input channels
+    :param num_classes -> specifies the number of output channels or masks for different classes
+    :param level_channels -> the number of channels at each level (count top-down)
+    :param bottleneck_channel -> the number of bottleneck channels 
+    :param device -> the device on which to run the model
+    -- forward()
+    :param input -> input Tensor
+    :return -> Tensor
+    """
     
-if __name__=="__main__":
-    # Example usage:
-    config = NetworkConfig(num_classes=3, input_channels=1)
-    model = Network(config)
-    input_tensor = torch.randn((1, 1, 64, 64, 64))  # Adjust input size as needed
-    output = model(input_tensor)
-    print("Output shape:", output.shape)
+    def __init__(self, in_channels=4, num_classes=4, level_channels=[64, 128, 256], bottleneck_channel=512) -> None:
+        super(Network, self).__init__()
+        level_1_chnls, level_2_chnls, level_3_chnls = level_channels[0], level_channels[1], level_channels[2]
+        self.a_block1 = Conv3DBlock(in_channels=in_channels, out_channels=level_1_chnls)
+        self.a_block2 = Conv3DBlock(in_channels=level_1_chnls, out_channels=level_2_chnls)
+        self.a_block3 = Conv3DBlock(in_channels=level_2_chnls, out_channels=level_3_chnls)
+        self.bottleNeck = Conv3DBlock(in_channels=level_3_chnls, out_channels=bottleneck_channel, bottleneck= True)
+        self.s_block3 = UpConv3DBlock(in_channels=bottleneck_channel, res_channels=level_3_chnls)
+        self.s_block2 = UpConv3DBlock(in_channels=level_3_chnls, res_channels=level_2_chnls)
+        self.s_block1 = UpConv3DBlock(in_channels=level_2_chnls, res_channels=level_1_chnls, num_classes=num_classes, last_layer=True)
+
+    
+    def forward(self, input):
+        #Analysis path forward feed
+        out, residual_level1 = self.a_block1(input)
+        out, residual_level2 = self.a_block2(out)
+        out, residual_level3 = self.a_block3(out)
+        out, _ = self.bottleNeck(out)
+
+        #Synthesis path forward feed
+        out = self.s_block3(out, residual_level3)
+        out = self.s_block2(out, residual_level2)
+        out = self.s_block1(out, residual_level1)
+        
+        out = F.interpolate(out, size=IMAGE_SIZE, mode='trilinear', align_corners=False)
+        return out
+
+
+
+if __name__ == '__main__':
+    #Configurations according to the Xenopus kidney dataset
+    # model = UNet3D(in_channels=4, num_classes=1)
+    # start_time = time.time()
+    # summary(model=model, input_size=(4, 240, 240, 155), batch_size=-1, device="cpu")
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = "cpu"
+    
+    model = Network(in_channels=4, num_classes=2)
+    model = model.to(device)  # Move the model to GPU
+    
+    input_tensor = torch.randn((1, 4, *IMAGE_SIZE)).to(device)  # Move the input tensor to GPU
+    start_time = time.time()
+    
+    # Ensure the model and input tensor are on the same device
+    assert model.parameters().__next__().device == input_tensor.device, "Model and input are not on the same device!"
+    
+    summary(model=model, input_size=(4, *IMAGE_SIZE), batch_size=-1, device=str(device))
+    print("--- %s seconds ---" % (time.time() - start_time))
